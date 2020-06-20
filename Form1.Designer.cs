e1e87633c3032fddfb155b36d061722e75f4317c
GJjoe <shorty01@gmail.com>
2020-06-20 11:19:48 -0230

    Initial Upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(280, 211);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(165, 37);
+            this.button1.TabIndex = 0;
+            this.button1.Text = " Run Script";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(25, 78);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(148, 21);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "Single Line Breaks";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(25, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(128, 38);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Load Script";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(25, 165);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(420, 22);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 143);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Loaded Script";
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Checked = true;
+            this.checkBox2.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox2.Location = new System.Drawing.Point(192, 78);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(274, 21);
+            this.checkBox2.TabIndex = 5;
+            this.checkBox2.Text = "Slow Send (do not use for CLR scripts)";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(192, 106);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDown1.TabIndex = 6;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(319, 110);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(146, 17);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Slow Send Delay (ms)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(488, 272);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "CSV Magic";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+//using WindowsInput;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+      //  string path;
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+      
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                DialogResult result = MessageBox.Show("Script will run 5 seconds after clicking Yes.\n\nPlease navigate to the desired text entry field" +
+                " within 5 seconds of clicking Yes to this prompt\n\nClick No to abort running this script.", "Run Script?", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+
+                if (result == DialogResult.Yes)
+                {
+                    if (textBox1.Text != "")
+                    {
+                        //   var sim = new InputSimulator();
+                        string whole_file = System.IO.File.ReadAllText(textBox1.Text);
+
+                        // Split into lines.
+                        if (checkBox1.Checked)
+                        {
+                            whole_file = whole_file.Replace(Environment.NewLine, "\n");
+                        }
+                        whole_file = whole_file.Replace("\n", "~");
+                        whole_file = whole_file.Replace(",", "~");
+                        whole_file = whole_file.Replace("#", "+{RIGHT}");
+                        whole_file = whole_file.Replace("$", ",");
+                        whole_file = whole_file.Replace("@", "{F10}");
+                        whole_file = whole_file.Replace("&", "{DOWN}");
+
+                        System.Threading.Thread.Sleep(5000);
+
+                        if (checkBox2.Checked)
+                        {
+
+                            foreach (char s in whole_file)
+                            {
+                                SendKeys.SendWait(s.ToString());
+                                System.Threading.Thread.Sleep(Convert.ToInt32(numericUpDown1.Value));
+                            }
+                        }
+
+                        else
+                        {
+                            SendKeys.SendWait(whole_file);
+                        }
+                        
+                        //SendKeys.SendWait(whole_file);
+
+                        MessageBox.Show("Scripting Complete");
+                        // SendKeys.SendWait("+{RIGHT}");
+                        //sim.Keyboard.ModifiedKeyStroke(WindowsInput.Native.VirtualKeyCode.RSHIFT, WindowsInput.Native.VirtualKeyCode.RIGHT);
+                        //System.Threading.Thread.Sleep(5000);
+                        //  SendKeys.Send("HELLO");
+
+                    }
+                   
+                }
+                
+            }
+            else 
+            {
+                MessageBox.Show("No Script Selected","Warning",MessageBoxButtons.OK, MessageBoxIcon.Error);
+                
+            }
+
+        }
+
+        void SendKeysSlowly(string whole_file)
+        {
+            foreach (char s in whole_file)
+            {
+                SendKeys.SendWait(s.ToString());
+                System.Threading.Thread.Sleep(50);
+
+            }
+        }
+
+       
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+             //openFileDialog1.InitialDirectory = "c:\\" ;
+    openFileDialog1.Filter = "Database files (*.csv)|*.csv" ;
+    openFileDialog1.FilterIndex = 0;
+    openFileDialog1.RestoreDirectory = true ;
+
+            if(openFileDialog1.ShowDialog() == DialogResult.OK)
+    {
+                var path = openFileDialog1.FileName;
+                MessageBox.Show("Loaded Script: \n" + path,"Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                textBox1.Text = path;
+   
+            }
+        }
+    }
+}
